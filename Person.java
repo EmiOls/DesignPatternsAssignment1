f89d7640251421962a9ff74e0e3d7d5a32fcf137
Emil Olsen <emiols2003@skola.goteborg.se>
2024-01-15 16:56:54 +0100

    auto format code

--- Person.java
+++ Person.java
@@ -1,15 +1,17 @@
 import java.io.Serializable;
-public class Person implements Serializable{
+
+public class Person implements Serializable {
   private static final long serialVersionUID = 3487495895819393L;
   private String name;
   private String email;
 
-  public Person(String name, String email){
+  public Person(String name, String email) {
     this.name = name;
-    this.email= email;
+    this.email = email;
   }
+
   @Override
-  public String toString(){
+  public String toString() {
     return String.format("%s - %s", name, email);
   }
 }
